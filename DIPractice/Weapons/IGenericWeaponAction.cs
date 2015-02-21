6413a28b2f97b67f7ac8fc583a0d349d729e37e7
Kristijan Horvat <kristijan@mono.hr>
2015-02-21 13:07:52 +0100

    The generic weapon action factory is added

---
+++ DIPractice/Weapons/IGenericWeaponAction.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DIPractice.Weapons
+{
+    public interface IGenericWeaponAction : IWeaponAction
+    {
+    }
+}
---
+++ DIPractice/Weapons/IGenericWeaponActionFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DIPractice.Weapons
+{
+    public interface IGenericWeaponActionFactory
+    {
+        #region Methods
+
+        //NOTE: This is Non-Abstract Factory, it is implementation specific and therefor anti-pattern
+
+        /// <summary>
+        /// Creates this instance.
+        /// </summary>
+        /// <param name="action">The action.</param>
+        /// <returns></returns>
+        IGenericWeaponAction Create(string action);
+
+        #endregion Methods
+    }
+
+    internal class GenericWeaponActionFactory : IGenericWeaponActionFactory
+    {
+        #region Methods
+
+        //NOTE: This is Non-Abstract Factory, it is implementation specific and therefor anti-pattern
+
+        /// <summary>
+        /// Creates this instance.
+        /// </summary>
+        /// <param name="action">The action.</param>
+        /// <returns></returns>
+        public IGenericWeaponAction Create(string action)
+        {
+            return new GenericWeaponAction(action);
+        }
+
+        #endregion Methods
+    }
+}
