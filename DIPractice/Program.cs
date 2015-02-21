b199b9984ef39029bdbb8472f79fd4cb6c86ed5c
Kristijan Horvat <kristijan@mono.hr>
2015-02-21 16:08:44 +0100

    Object Scopes - Thread Scope

--- DIPractice/Program.cs
+++ DIPractice/Program.cs
@@ -2,12 +2,27 @@
 using DIPractice.Weapons;
 using Ninject;
 using System;
+using System.Threading;
 
 namespace DIPractice
 {
     public class Program
     {
         #region Methods
+
+        private static void Attack(IKernel kernel)
+        {
+            Console.WriteLine("Thread id: {0}", Thread.CurrentThread.ManagedThreadId);
+            var warrior1 = kernel.Get<IWarrior>();
+            warrior1.Attack("the evildoers");
+
+            Console.WriteLine("Who do you want to kill today?");
+            Console.Out.Flush();
+            var name = Console.ReadLine();
+
+            var warrior2 = kernel.Get<IWarrior>();
+            warrior2.Attack(name);
+        }
 
         /// <summary>
         /// Mains the specified arguments.
@@ -25,17 +40,28 @@ namespace DIPractice
 
             kernel.Bind<IWeaponFactory>().To<WeaponFactory>();
 
-            kernel.Bind<IWarrior>().To<Samurai>().InSingletonScope();
+            kernel.Bind<IWarrior>().To<Samurai>().InThreadScope();
 
-            var warrior1 = kernel.Get<IWarrior>();
-            warrior1.Attack("the evildoers");
+            StartNewThread(() =>
+                {
+                    Attack(kernel);
+                }).Join();
 
-            Console.WriteLine("Who do you want to kill today?");
-            Console.Out.Flush();
-            var name = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("We are bloodthirsty today ...");
+            Console.WriteLine();
 
-            var warrior2 = kernel.Get<IWarrior>();
-            warrior2.Attack(name);
+            StartNewThread(() =>
+            {
+                Attack(kernel);
+            });
+        }
+
+        private static Thread StartNewThread(Action a)
+        {
+            Thread t = new Thread(new ThreadStart(a));
+            t.Start();
+            return t;
         }
 
         #endregion Methods
